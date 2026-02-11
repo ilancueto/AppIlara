a0a2b94b809cf54a277f3f12676b2241c9b10d59
ilancueto <65678462+ilancueto@users.noreply.github.com>
2026-02-11 17:12:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -431,7 +431,7 @@ st.markdown(
       box-shadow: 0 12px 30px rgba(0,0,0,.14);
       margin-bottom: 12px;">
 
-      <img src="https://raw.githubusercontent.com/ilancueto/AppIlara/main/assets/logo_ilara.png" style="
+      <img src="https://raw.githubusercontent.com/ilancueto/AppIlara/main/assets/logo_icon.png" style="
         height:64px;
         border-radius:50%;
         background:white;
@@ -1209,6 +1209,7 @@ Que cada venta te acerque a lo que soñás, y que nunca te falten motivos para s
 **Te amo.**  
 — Ilan
 """)
+
 
 
 
